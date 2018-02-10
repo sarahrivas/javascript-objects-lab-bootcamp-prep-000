25aaeebec95061e473bf5041496d62aaef73051c
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-02-10 19:40:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  recipes[key] = value;
+  object[key] = value;
   return recipes;
 }
 
