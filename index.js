9f29f978d169194ad252003b4adcffdbe34840f0
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-02-10 19:37:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return newObj;
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  recipes[key] = value;
-  return recipes;
-}
+// function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+//   recipes[key] = value;
+//   return recipes;
+// }
 
-function destructivelyDeleteFromObjectByKey(object, key) {
-  delete recipes[key];
-  return recipes;
-}
+// function destructivelyDeleteFromObjectByKey(object, key) {
+//   delete recipes[key];
+//   return recipes;
+// }
 
-function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, recipes);
-  delete newObj[key];
-  return newObj;
-}
+// function deleteFromObjectByKey(object, key) {
+//   var newObj = Object.assign({}, recipes);
+//   delete newObj[key];
+//   return newObj;
+// }
