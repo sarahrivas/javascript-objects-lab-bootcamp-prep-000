1cf46daae8fbe18798e3a4d9c767488b4fbc0917
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-02-10 19:41:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function destructivelyDeleteFromObjectByKey(object, key) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, recipes);
+  var newObj = Object.assign({}, object);
   delete newObj[key];
   return newObj;
 }
